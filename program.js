50748a24e23f5008e4965abc4910bb47e21c22f6
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-09-03 20:53:55 +0100

    Remove testing for getComputerChoice()

--- program.js
+++ program.js
@@ -1,13 +1,9 @@
 function getComputerChoice() {
     let computerChoice = 0;
     computerChoice = Math.random();
-    console.log(computerChoice);
     computerChoice *= 1000;
-    console.log(computerChoice);
     computerChoice = Math.floor(computerChoice);
-    console.log(computerChoice);
     computerChoice = computerChoice % 3;
-    console.log(computerChoice);
     if (computerChoice == 0) {
         return "rock";
     } else if (computerChoice == 1) {
