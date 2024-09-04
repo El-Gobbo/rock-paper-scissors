a97ee5d3a98efb032b0f173c9adba1a4e510ab05
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-09-04 19:34:51 +0100

    Implement playRound() function

--- program.js
+++ program.js
@@ -24,7 +24,7 @@ function getHumanChoice() {
         }
     }
 
-    let humanChoice = 0;
+    let humanChoice = null;
     let loopCount = 0;
     do {
         if (loopCount > 0) {alert("This is not a valid choice")};
@@ -33,8 +33,36 @@ function getHumanChoice() {
         if (humanChoice == null) {return "Cancel"}
         else {humanChoice = humanChoice.toLowerCase()};
         loopCount = 1;
+
     }
     while (!checkIfValid(humanChoice));
 
     return humanChoice
 }
+
+function playRound(humanChoice, computerChoice) {
+    let resultMessage;
+    if (humanChoice === computerChoice) {
+        ++humanScore;
+        ++computerScore;
+        resultMessage = `You drew!`;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "rock") {
+        ++humanScore;
+        resultMessage = `You win! ${humanChoice} beats ${computerChoice}.`;
+        }
+    else {++computerScore;
+        resultMessage = `You lose! ${humanChoice} is beaten by ${computerChoice}.`;
+    }
+    return console.log(resultMessage);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
