3b241fea1b7794e4d7d55254f141a920d8ab6321
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-09-08 11:09:47 +0100

    Add div around all game-related elements
    This will allow me to hide the game-related
    elements while the game is not running.

--- program.js
+++ program.js
@@ -3,7 +3,9 @@ const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const outcome = document.querySelector("#results");
 const buttonDiv = document.querySelector("#buttonDiv");
-const startButton = document.querySelector('#startGame')
+const startButton = document.querySelector('#startGame');
+const cScoreNumber = document.querySelector("#cScoreNumber");
+const hScoreNumber = document.querySelector("#hScoreNumber");
 
 startButton.addEventListener('click',playGame, {once: true});
 
@@ -22,7 +24,6 @@ buttonDiv.addEventListener('click', (e) => {
             break;
     }
     let outcome = playRound(cSelection,hSelection);
-    alert(outcome);
 
 })
 
@@ -34,23 +35,6 @@ function getCChoice() {
     } else if (cChoice == 1) {
         return "Paper";
     } else {return "Scissors"};
-}
-
-function getHChoice() {
-    let hChoice = null;
-    let loopCount = 0;
-    do {
-        if (loopCount > 0) {alert("This is not a valid choice")};
-        hChoice = prompt("Choose from rock, paper, or scissors");
-        if (hChoice == null) {return "Forfeit"}//To account for cancelling the prompt
-        else {hChoice = (hChoice.at(0).toUpperCase() + hChoice.slice(1))};//To allow case-insensitive inputs
-        loopCount = 1;//to trigger an alert message warning of incorrect input
-    }
-    while (hChoice !== "Rock" 
-        && hChoice !== "Paper" 
-        && hChoice !== "Scissors");
-
-    return hChoice
 }
 
 function playRound(hChoice, cChoice) {
@@ -69,45 +53,50 @@ function playRound(hChoice, cChoice) {
 }
 
 function playGame() {
+    function updateScore (){
+        cScoreNumber.textContent = cScore;
+        hScoreNumber.textContent = hScore;
+    }
     startButton.textContent = `Game in progress`;
     let hScore = 0;
     let cScore = 0;
     let resultMessage = "";
     let cSelection = '';
     let hSelection = '';
-    while (hScore < 5 && cScore < 5) {
-        buttonDiv.addEventListener('click', (e) => {
-            cSelection = getCChoice();
-            hSelection = '';
-            switch (e.target.id){
-                case 'rock':
-                    hSelection = 'Rock';
-                    break;
-                case 'paper':
-                    hSelection = 'Paper';
-                    break;
-                case 'scissors':
-                    hSelection = 'Scissors';
-                    break;
-            }
-            let result = playRound(hSelection, cSelection);
-            switch(result){
-                case 'win':
-                    ++hScore;
-                    resultMessage = `You win! ${hSelection} beats ${cSelection}.`;
-                    break;
-                case 'draw':
-                    ++hScore;
-                    ++cScore;
-                    resultMessage = `You drew!`;
-                    break;
-                case `lose`:
-                    ++cScore;
-                    resultMessage = `You lose! ${hSelection} is beaten by ${cSelection}.`
-                    break;
+    updateScore();
+    buttonDiv.addEventListener('click', (e) => {
+        cSelection = getCChoice();
+        hSelection = '';
+        switch (e.target.id){
+            case 'rock':
+                hSelection = 'Rock';
+                break;
+            case 'paper':
+                hSelection = 'Paper';
+                break;
+            case 'scissors':
+                hSelection = 'Scissors';
+                break;
         }
+        let result = playRound(hSelection, cSelection);
+        switch(result){
+            case 'win':
+                ++hScore;
+                resultMessage = `You win! ${hSelection} beats ${cSelection}.`;
+                break;
+            case 'draw':
+                ++hScore;
+                ++cScore;
+                resultMessage = `You drew!`;
+                break;
+            case `lose`:
+                ++cScore;
+                resultMessage = `You lose! ${hSelection} is beaten by ${cSelection}.`
+                break;
+        }
+        updateScore();
         outcome.textContent = resultMessage;
         
-        });
-    }
+    });
+    
 }
